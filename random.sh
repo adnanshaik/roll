74389842549c3f8e762831ab24f1b3ecec60c17c
adnanshaik <68059262+adnanshaik@users.noreply.github.com>
2020-11-13 14:05:03 +0530

    Update random.sh

--- random.sh
+++ random.sh
@@ -2,20 +2,20 @@
 
 while [ 1 ]
 do
-n=$((RANDOM%10000))     				#Generates 4 digit random number
-((a++))
-count=0
-for ((i=1;i<=n;i++))                                   #checks the condition for prime number
-do
-	if [ $((n%i)) -eq 0 ]
+     n=$((RANDOM%10000))     				#Generates 4 digit random number
+     ((a++))
+     count=0
+  for ((i=1;i<=n;i++))                                   #checks the condition for prime number
+  do
+ 	if [ $((n%i)) -eq 0 ]
 	then						
 	  ((count++))                                    
 	fi
-	done
-	if [ $count -eq 2 ]
-	then
-	   echo "$n is a prime num"
-	   echo "num of count:"$a
-	break
-	fi
-	done
+  done
+           if [ $count -eq 2 ]
+           then
+               echo "$n is a prime num"
+               echo "num of count:"$a
+             break
+           fi
+done
