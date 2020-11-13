7d11ac226c0b3104816f68a5bb953f238f797157
adnanshaik <68059262+adnanshaik@users.noreply.github.com>
2020-11-13 13:07:10 +0530

    Update roll2.sh

--- roll2.sh
+++ roll2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -x
 
 sum=0
-until [ $r1 -eq 6 ] && [ $r2 -eq 6 ]
-do
+until [[ $r1 -eq 6  &&  $r2 -eq 6 ]]         #runs until both dice becomes six.
+do 
 ((count++))
 r1=$((RANDOM%6+1))
 r2=$((RANDOM%6+1))
-sum=$((sum+r2+r1))
+sum=$((sum+r2+r1))                           #calculates the sum of both the dice until it becomes six.
 done
-
 echo "num of rolls:"$count
 echo "sum of rolls:"$sum
+
 
 
